a0191e9ea21fac7926c84f44b97c8bfdf173fe1d
Lukas Hein <lukashein@protonmail.com>
2022-01-05 19:51:24 +0100

    Setup node

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import browser from 'webextension-polyfill';
+
 let query = null;
 let tabId = null;
 
--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import browser from 'webextension-polyfill';
+
 browser.runtime.onMessage.addListener(request => {
   document.getElementsByName("j")[0].value = request.query;
   document.getElementsByName("search")[0].click();
